7af9f50ac2a686bccd364309a35874372879c23a
Chris Dziewa <chris.dziewa@gmail.com>
2015-07-28 16:33:23 -0500

    write test for downcased save of email in database

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -65,4 +65,11 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "password should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
